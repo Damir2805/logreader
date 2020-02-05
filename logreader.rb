3a81adb053879ea6c9c041b66881489cdc75deda
Dammir2805 <d_kalymbaev@mail.ru>
2020-02-05 19:54:44 +0300

    final version

--- logreader.rb
+++ logreader.rb
@@ -1,38 +1,40 @@
 # This program is to alert about NOK events
 
-#We should define the file path to events.log
-#file_path = "./Rubyprojects/Logreader/events.log"
-
-
-
 #Infinitive loop of reading log file
 loop do 
 
-	file_path = "./Data/events.log"
+	file_path1 = "./Data/events.log"
+	file_path2 = "./Data/alerts"
 
-	times = [0]
-	
-	content = File.new(file_path, "r:UTF-8")
+	#Reading the log file	
+	content = File.new(file_path1, "r:UTF-8")
 	f = content.read.downcase.split
 
 	results = f.select{|item| item.include?("nok")}
 	nok_counter = results.size
-	times << nok_counter
-	times1 = times.reverse
+
+	#Adding data into a different file
+	alerts = File.new(file_path2, "a:UTF-8")
+	alerts.print "#{nok_counter}\n\r"
+	alerts.close
+
+	#Reading recorded data and counting NOK events
+	alerts = File.new(file_path2, "r:UTF-8")
+	als = alerts.read.split.compact.reverse
+	
+	alerts.close
+
+	#Counting difference between two last numbers which shows a number
+	#of NOK events
+	a1 = als[0].to_i
+	a2 = als[1].to_i
+
+	alerts_counter = a1 - a2
 
 	puts
-	puts times
-	puts times1
-	puts
-
-	#times_per_minuite = (times1[2] - times1[1])
-
-	#puts f
-	puts nok_counter
-	#puts times_per_minuite
+	
+	puts "There were #{alerts_counter} NOK events per last minute"
 
 	sleep 20	
 
 end
-#I need to add an element to the array in the tail.
-#And I need delete the first element in the array
